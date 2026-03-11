b9160594a5ef1b07a7ab117e7eb0ab33e80413fe
Pedro Goncalves Rodrigues <psypg3@nottingham.ac.uk>
2026-03-11 11:25:04 +0000

    Added JS timer and period funciotnality

--- index.js
+++ index.js
@@ -4,6 +4,75 @@ let guestPoints = 0;
 let homeFouls = 0;
 let guestFouls = 0;
 
+let period = 0;
+let timerSec = 4 //720
+let timerEl = document.getElementById("Timer")
+
+let periodOver = 1
+let gameOver = 0
+
+setInterval(TimerDec, 1000)
+
+function ConvertSecsToTimer(){
+    let result = Math.trunc(timerSec/60)+":"+(timerSec%60)
+    return result
+}
+
+function TimerDec(){
+    console.log("In Timer fn")
+    if((periodOver || gameOver) != true){
+        console.log("Oops")
+        timerSec--
+        timerEl.textContent = ConvertSecsToTimer()
+
+        if(timerSec <= 0){ //if period is over
+            if(period == 4){ //check if last period
+                GameEnd()
+                return
+            }
+            periodOverFn() 
+        }
+        UpdateWinningMarker()
+    }
+   
+
+
+}
+function UpdateWinningMarker(){
+
+}
+
+function unPause(){
+    periodOver = 0
+    timerSec = 5 //720
+    //change HTML elems
+}
+
+function periodOverFn(){
+    periodOver = 1
+    period++
+    let periodCounter = document.getElementById("Period")
+    periodCounter.textContent = period+"/4"
+}
+
+function GameEnd(){
+    //Stop timer/period/points updating
+
+    let buttonEl = document.getElementById("addOneHome")
+    buttonEl.onclick = ""
+    buttonEl = document.getElementById("addTwoHome")
+    buttonEl.onclick = ""
+    buttonEl = document.getElementById("addThreeHome")
+    buttonEl.onclick = ""
+    
+    buttonEl = document.getElementById("addOneGuest")
+    buttonEl.onclick = ""
+    buttonEl = document.getElementById("addTwoGuest")
+    buttonEl.onclick = ""
+    buttonEl = document.getElementById("addThreeGuest")
+    buttonEl.onclick = ""
+    //update reset button fn call
+}
 
 function AddToPoints(Points, team){
     if(team == "home"){
